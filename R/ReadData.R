52a4baa6d8a97031168587e5184f3d130948724d
Thomas Witzani <thomaswitzani@gmx.de>
2024-10-31 13:35:53 +0100

    corrected data reading

--- R/ReadData.R
+++ R/ReadData.R
@@ -3,9 +3,9 @@ library(dplyr)
 library(tidyr)
 
 data_fcmstress_raw <- read.csv("data/FCM Stress - Collared Deer - CRS=ETRS UTM 33N.csv", 
-                               header = TRUE, sep = ";")
+                               header = TRUE, sep = ",")
 data_huntevents_raw <- read.csv("data/Hunt Events - CRS= ETRS UTM 33N.csv", 
-                                header = TRUE, sep = ";")
+                                header = TRUE, sep = ",")
 data_movement_raw <- read.csv("data/Movement - CRS=ETRS UTM 33N.csv", 
                               header = TRUE, sep = ";")
 
