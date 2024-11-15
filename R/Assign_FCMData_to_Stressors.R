3fba3e1fed081c3eed1fb5ac4582a8f0d121aed3
Thomas Witzani <thomaswitzani@gmx.de>
2024-11-15 15:27:13 +0100

    fixed a bunch of stuff

--- R/Assign_FCMData_to_Stressors.R
+++ R/Assign_FCMData_to_Stressors.R
@@ -9,14 +9,13 @@ FCMData_Assigned <- data.frame(Sender.ID = integer(),
                       HairID = character(),
                       Defec_Time = as.POSIXct(character()),
                       Stress_Time = as.POSIXct(character()),
-                      X_fcm = numeric(),
-                      Y_fcm = numeric(),
                       Speed = numeric(),
                       Distance = numeric(),
                       ng_g = numeric(),
                       StressorID = integer(),
                       stringsAsFactors = FALSE)
 cat("\n")
+
 # Iterate through each row in FCMStress
 for (i in 1:nrow(FCMStress)) {
   cat(sprintf("FCM Assignment: %d of %d", i, nrow(FCMStress)), "\r")
@@ -32,8 +31,8 @@ for (i in 1:nrow(FCMStress)) {
   
   matching_events <- StressEvents %>%
     filter(Sender.ID == sender_id &
-             difftime(HuntEventTime, collar_time, units = "hours") >= 16 &
-             difftime(HuntEventTime, collar_time, units = "hours") <= 21)
+             difftime(collar_time, HuntEventTime, units = "hours") >= 16 &
+             difftime(collar_time, HuntEventTime, units = "hours") <= 21)
   
   # If there are matching events, create entries in the FCMData_Assigned data frame
   if (nrow(matching_events) > 0) {
@@ -44,41 +43,26 @@ for (i in 1:nrow(FCMStress)) {
                               HairID = hair_id,
                               Defec_Time = collar_time,
                               Stress_Time = event_row$HuntEventTime,
-                              X_fcm = x_fcm,
-                              Y_fcm = y_fcm,
-                              Speed = event_row$Speed,
+                              TimeDiff = difftime(collar_time, event_row$HuntEventTime, units = "hours"),
                               Distance = event_row$Distance,
                               ng_g = ng_g,
                               StressorID = event_row$StressorID,
                               stringsAsFactors = FALSE)
       
-      # Append the new entry to the FCMData_Assigned data frame
       FCMData_Assigned <- rbind(FCMData_Assigned, new_entry)
     }
   }
 }
 
-Data_Clean <- FCMData_Assigned %>%
+Samples_Relevant <- FCMData_Assigned %>%
   group_by(Sample_ID) %>%
   summarise(
     ng_g = mean(ng_g, na.rm = TRUE),
-    distance = min(Distance, na.rm = TRUE)  # Assuming you also want the average distance per Sample_ID
+    distance = min(Distance, na.rm = TRUE)
   )
 
-# Data_Clean <- Data_Clean %>%
-#   filter(distance < 5000)
+# Samples_Relevant <- Samples_Relevant %>%
+#    filter(distance < 2000)
 
 
-lm_model <- lm(ng_g ~ distance, data = Data_Clean)
-
-# Step 3: Plot the data with the linear model fit
-ggplot(Data_Clean, aes(x = distance, y = ng_g)) +
-  geom_point() +                                  # Scatter plot of the averages
-  geom_smooth(method = "lm", se = TRUE, color = "blue") +  # Add linear model fit line with confidence interval
-  labs(title = "Average Distance vs. Average ng_g with Linear Model Fit",
-       x = "Distance",
-       y = "ng_g") +
-  theme_minimal()
-
-summary(lm_model)
 
