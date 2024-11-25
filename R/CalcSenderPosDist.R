4445372327ce580f70f3e4775187901f2e85f628
b-zhou <baisu.zhou@outlook.com>
2024-11-25 17:03:24 +0100

    compute distances faster

--- R/CalcSenderPosDist.R
+++ R/CalcSenderPosDist.R
@@ -1,68 +1,66 @@
-library(data.table)
-library(sf)
-
 #getwd()
-source("./R/Datafusion.R")
-
-MovementDT <- as.data.table(Movement)
-setkey(MovementDT, Sender.ID, t_)
-HuntEventsDT <- as.data.table(HuntEventsreduced)
-
-interpolated_positions <- rbindlist(lapply(1:nrow(HuntEventsDT), function(i) {
-  hunt_time <- HuntEventsDT$t_[i]
-  hunt_x <- HuntEventsDT$X[i]
-  hunt_y <- HuntEventsDT$Y[i]
-  
-  # Filter rows only for Sender.IDs with timestamps around the hunting event
-  relevant_data <- MovementDT[
-    , .SD[t_ <= hunt_time | t_ >= hunt_time], by = Sender.ID]
-  
-  # Sort relevant_data by time to ensure accuracy
-  setorder(relevant_data, t_)
-  
-  # Calculate the closest rows before and after the event per Sender.ID
-  before_event <- relevant_data[, .SD[t_ <= hunt_time][.N], by = Sender.ID]
-  after_event <- relevant_data[, .SD[t_ >= hunt_time][1], by = Sender.ID]
-  
-  # Merge before and after events, renaming columns appropriately
-  merged_events <- merge(before_event, after_event, by = "Sender.ID", suffixes = c("_before", "_after"))
-  
-  # Perform interpolation
-  interpolated <- merged_events[
-    !is.na(t__before) & !is.na(t__after), 
-    .(
-      Sender.ID,
-      HuntEventTime = hunt_time,
-      HuntEventX = hunt_x,
-      HuntEventY = hunt_y,
-      InterpolatedX = x__before + (as.numeric(difftime(hunt_time, t__before, units = "secs")) / 
-                                     as.numeric(difftime(t__after, t__before, units = "secs"))) * (x__after - x__before),
-      InterpolatedY = y__before + (as.numeric(difftime(hunt_time, t__before, units = "secs")) / 
-                                     as.numeric(difftime(t__after, t__before, units = "secs"))) * (y__after - y__before)
-    )
-  ]
-  
-  interpolated
-}))
+# source("./R/Datafusion.R")
 
 
-# Convert the result back to a data.frame
-interpolated_positions <- as.data.frame(interpolated_positions)
-
-#Kick out rows with NAs
-StressEvents <- na.omit(interpolated_positions)
-
-# Calculate the distances and assign StressorID
-StressEvents <- StressEvents %>%
-  rowwise() %>%
-  mutate(
-    Distance = st_distance(
-      st_sfc(st_point(c(HuntEventX, HuntEventY)), crs = 32633),
+# Parameters:
+# - `data_to_locate`: tibble with columns "Sender.ID", "Hunt.ID"
+get_distances <- function(data_to_locate, folder = "Data") {
+  # library(data.table)
+  library(sf)
+  
+  Movement <- read_movement(folder)
+  HuntEvents <- read_hunting(folder)
+  
+  # get hunt event details
+  data_to_locate <- data_to_locate %>%
+    left_join(HuntEvents, by = "Hunt.ID") %>%
+    rename(HuntX = X, HuntY = Y,
+           HuntDate = Date, HuntTime = t_, HuntTimeMissing = TimeMissing)
+  
+  # If timestamp exsits, use linear interpolation to find the location of the
+  # deer at the time of hunting
+  data_with_time <- data_to_locate %>%
+    filter(!is.na(HuntTime)) %>%
+    distinct() %>%  # just in case of duplicates
+    # find deer location just before hunting event
+    left_join(Movement, join_by(Sender.ID, closest(HuntTime >= t_))) %>%
+    rename(x_before = x_, y_before = y_, t_before = t_) %>%
+    # find deer location just after hunting event
+    left_join(Movement, join_by(Sender.ID, closest(HuntTime <= t_))) %>%
+    rename(x_after = x_, y_after = y_, t_after = t_) %>%
+    # interpolate
+    mutate(
+      time_total = as.numeric(difftime(t_after, t_before), units = "secs"),
+      time_traversed = as.numeric(difftime(HuntTime, t_before, units = "secs")),
+      InterpolatedX = x_before +
+        time_traversed / time_total * (x_after - x_before),
+      InterpolatedY = y_before +
+        time_traversed / time_total * (y_after - y_before)
+    ) %>%
+    # if location before hunting event == location after hunting event,
+    # no interpolation is needed
+    mutate(
+      InterpolatedX = ifelse(time_total == 0, x_after, InterpolatedX),
+      InterpolatedY = ifelse(time_total == 0, y_after, InterpolatedY)
+    ) %>%
+    # calculate distance
+    rowwise() %>%
+    mutate(Distance = st_distance(
+      st_sfc(st_point(c(HuntX, HuntY)), crs = 32633),
       st_sfc(st_point(c(InterpolatedX, InterpolatedY)), crs = 32633)
-    )
-  ) %>%
-  ungroup() %>%
-  mutate(StressorID = row_number())
-StressEvents$Distance <- as.numeric(StressEvents$Distance)
+    )[[1]]) %>%
+    select(Sender.ID, Hunt.ID, HuntDate, HuntTime, HuntTimeMissing, Distance)
 
-View(StressEvents)
+  # If timestamp does not exist, find average distance on the day of hunting event.
+  # TBD: compute the average distance from the deer to the hunting event
+  # on the day of the hunting event
+  data_without_time <- data_with_time[0, ]
+  
+  data_all <- full_join(
+    data_with_time,
+    data_without_time,
+    join_by(Sender.ID, Hunt.ID, HuntDate, HuntTime, HuntTimeMissing, Distance)
+  )
+  
+  return(data_all)
+}
