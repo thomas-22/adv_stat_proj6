6f674117a0af03f841cbc248eee3449f9d9447f3
b-zhou <baisu.zhou@outlook.com>
2024-11-25 17:02:34 +0100

    data loading functions

--- R/Datafusion.R
+++ R/Datafusion.R
@@ -3,18 +3,22 @@ library(dplyr)
 library(lubridate)
 library(readxl)
 
+last_relevant_hunt_timediff_max <- 30 # hours
+last_relevant_hunt_timediff_min <- 16 # hours
+last_relevant_hunt_distance <- 3 # km
 
-# Read all datasets
+# Helper functions to load and pre-process datasets
 # 
 # Parameters:
 # - folder: character(1), folder of the csv/xlsx files
 # 
-# returns: a named list of data.frames `FCMStress`, `Movement`, `HuntEvents`
-read_all_data <- function(folder = "Data") {
+# returns: tibble `FCMStress`, `Movement`, or `HuntEvents`
+read_fcm <- function(folder = "Data") {
   # read FCM data
   path_fcm <- file.path(folder,
                         "FCM Stress - Collared Deer - CRS=ETRS UTM 33N.csv")
-  FCMStress <- read_delim(path_fcm) %>%
+  
+  FCMStress <- read_delim(path_fcm, show_col_types = FALSE) %>%
     as_tibble() %>%
     # only keep female deer
     # other entries are errors
@@ -39,6 +43,7 @@ read_all_data <- function(folder = "Data") {
   # column pregnant is time interval
   path_production <- file.path(folder, "Reproduction Success Results.xlsx")
   ReproductionSuccess <- read_excel(path_production) %>%
+    as_tibble() %>%
     filter(!is.na(`birth date`)) %>%
     mutate(birth_date = ymd(`birth date`),
            Sender.ID = factor(`Collar ID`, levels = sender_ids),
@@ -55,30 +60,79 @@ read_all_data <- function(folder = "Data") {
     mutate(pregnant = (!is.na(pregnant_since)) & (!is.na(pregnant_to))) %>%
     select(-pregnant_since, -pregnant_to)
   
+  return(FCMStress)
+}
+
+read_movement <- function(folder = "Data", fcm_data = read_fcm(folder)) {
+  sender_ids <- levels(fcm_data$Sender.ID)
   
-  # read movement data
-  first_date <- date(first(FCMStress$DefecTime) - hours(50))
-  last_date <- date(last(FCMStress$DefecTime))
+  first_date <- date(first(fcm_data$DefecTime) - hours(50))
+  last_date <- date(last(fcm_data$DefecTime))
   
   path_movement <- file.path(folder, "Movement - CRS=ETRS UTM 33N.csv")
   Movement <- readr::read_delim(path_movement, delim = ";") %>%
+    as_tibble() %>%
     mutate(Sender.ID = factor(Sender.ID, levels = sender_ids),
            t_ = parse_date_time(t_, orders = "%d/%m/%Y %h:%M")) %>%
     filter(date(t_) >= first_date, date(t_) <= last_date) %>%
     select(Sender.ID, x_, y_, t_)
   
-  
-  # read HuntEvents
-  path_hunt <- file.path(folder, "Hunt Events - CRS= ETRS UTM 33N.csv")
-  HuntEvents <- read_delim(path_hunt, delim = ",") %>%
-    mutate(t_ = stringr::str_c(Datum, Zeit, sep = " ") %>%
-             parse_date_time(orders = "%d/%m/%Y %h:%M:%s")) %>%
-    mutate(Date = dmy(Datum)) %>%
-    select(Date, X, Y, t_)
-  
-  
-  list(FCMStress = FCMStress, Movement = Movement, HuntEvents = HuntEvents)
+  return(Movement)
 }
+
+read_hunting <- function(folder = "Data", fcm_data = read_fcm(folder),
+                         with.time = NULL) {
+  first_date <- date(first(fcm_data$DefecTime) - hours(50))
+  last_date <- date(last(fcm_data$DefecTime))
+  
+  path_hunt <- file.path(folder, "Hunt Events - CRS= ETRS UTM 33N.csv")
+  HuntEvents <- read.csv(path_hunt, sep = ",") %>%
+    as_tibble() %>%
+    mutate(
+      TimeMissing = is.na(Zeit),
+      t_ = stringr::str_c(Datum, Zeit, sep = " ") %>%
+        parse_date_time(orders = "%d/%m/%Y %h:%M:%s"),
+      Date = dmy(Datum)
+    ) %>%
+    filter(Date >= first_date, Date <= last_date) %>%
+    select(Date, X, Y, t_, TimeMissing) %>%
+    distinct() %>%
+    mutate(Hunt.ID = row_number())
+  
+  if (is.null(with.time)) {
+    return(HuntEvents)
+  }
+  if (with.time) {
+    return(HuntEvents %>% filter(!TimeMissing))
+  }
+  return(HuntEvents %>% filter(TimeMissing))
+}
+
+# read_hunting_reduced <- function(folder = "Data", fcm_data = read_fcm(folder)) {
+#   na.omit(read_hunting(folder), fcm_data)
+# }
+# 
+# read_hunting_imputed <- function(folder = "Data", fcm_data = read_fcm(folder)) {
+#   first_date <- date(first(fcm_data$DefecTime) - hours(50))
+#   last_date <- date(last(fcm_data$DefecTime))
+#   
+#   path_hunt <- file.path(folder, "Hunt Events - CRS= ETRS UTM 33N.csv")
+#   # read.delim does not work here
+#   HuntEvents_imputed <- read.csv(path_hunt, sep = ",") %>%
+#     as_tibble() %>%
+#     # impute with 00:00:01
+#     mutate(
+#       TimeMissing = is.na(Zeit),
+#       Zeit = ifelse(TimeMissing, "00:00:01", Zeit),
+#       t_ = stringr::str_c(Datum, Zeit, sep = " ") %>%
+#         parse_date_time(orders = "%d/%m/%Y %h:%M:%s"),
+#       Date = dmy(Datum)
+#     ) %>%
+#     filter(Date >= first_date, Date <= last_date) %>%
+#     select(Date, X, Y, t_)
+#   
+#   return(HuntEvents_imputed)
+# }
 
 
 # What to do with NA's? here: just drop the lines. Drop any duplicate entries.
