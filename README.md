1045934d43a6d7339ac23d58ec22fe8c91494fa5
Nikolai German <129080480+NikoGerman@users.noreply.github.com>
2024-12-02 09:57:36 +0100

    whatever

--- README.md
+++
@@ -1,2 +0,0 @@
-# adv_stat_proj6
-https://moodle.lmu.de/course/view.php?id=35883
--- source_all.R
+++
@@ -1,2 +0,0 @@
-source("settings.R")
-lapply(rev(list.files("R/", pattern = "\\.R$", full.names = TRUE, recursive = TRUE)), source)
