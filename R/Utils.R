0bf47f9bd048e5bb6e5e8f578df4c340a69ab5d9
b-zhou <baisu.zhou@outlook.com>
2025-01-10 18:21:32 +0100

    copy over utils

---
+++ R/Utils.R
@@ -0,0 +1,27 @@
+get_season <- function(datetimes) {
+  res <- vapply(
+    datetimes,
+    function(datetime) {
+      if (month(datetime) %in% c(3, 4, 5)) return("spring")
+      if (month(datetime) %in% c(6, 7, 8)) return("summer")
+      if (month(datetime) %in% c(9, 10, 11)) return("autumn")
+      if (month(datetime) %in% c(12, 1, 2)) return("winter")
+    },
+    character(1)
+  )
+  factor(res, levels = c("spring", "summer", "autumn", "winter"))
+}
+
+
+get_herds <- function(Movement.data, enclosures) {
+  # takes 
+  # > Movement data
+  # > enlosures: a numeric vector containing a x and y coordinate for every enclosure center
+  # omits rows with NAs in movement data
+  # gives x and y coordinates to kmeans() together with enlcosures as centers
+  # returns movement.data augmented with a vector containg the group number
+  Movement.data <- na.omit(Movement.data)
+  Position <- Movement.data %>% select(x_, y_)
+  km.out <- kmeans(Position, centers = enclosures)
+  cbind(Movement.data, group = as.factor(km.out$cluster))
+}
