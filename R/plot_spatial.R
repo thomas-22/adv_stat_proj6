007651c04185e3dc19f3c63cdef1cf18ef6a23d8
b-zhou <baisu.zhou@outlook.com>
2025-01-27 21:29:19 +0100

    remove blue region from spatial plots, add new movement plot

--- R/plot_spatial.R
+++ R/plot_spatial.R
@@ -1,25 +1,42 @@
-library(sf)
+library(readr)
+library(stringr)
 library(dplyr)
+library(tidyr)
+library(lubridate)
+library(readxl)
+library(ggplot2)
+library(sf)
 library(osmdata)
 library(ggspatial)
 library(patchwork)
-### construct sf
-Movement_sf <- read.csv("data/Movement - CRS=ETRS UTM 33N.csv", 
-                        header = TRUE, sep = ";") %>%
-  select(Sender.ID, x_, y_) %>%
-  st_as_sf(coords = c("x_", "y_"), crs = 32633)
 
-HuntEvents_sf <- read.csv("data/Hunt Events - CRS= ETRS UTM 33N.csv", 
-                          header = TRUE, sep = ",") %>%
-  select(X, Y) %>% 
-  filter(Y < max(Y) & Y > min(Y)) %>%
-  st_as_sf(coords = c("X", "Y"), crs = 32633)
+source("R/Datafusion.R")
+prepared_data <- suppressWarnings(run_datafusion(remove_outlers = TRUE))
+
+Movement <- prepared_data$Movement
+FCMStress <- prepared_data$FCMStress
+HuntEvents <- prepared_data$HuntEvents
+
+### construct sf
+Movement_sf <- Movement %>%
+  dplyr::select(Sender.ID, x_, y_) %>%
+  st_as_sf(coords = c("x_", "y_"), crs = 25833)
+
+set.seed(42)
+Movement_sf_selected <- Movement %>%
+  dplyr::select(Sender.ID, x_, y_) %>%
+  # select randomly 3 deer to prevent overplotting
+  filter(Sender.ID %in% sample(unique(Movement$Sender.ID), 4)) %>%
+  st_as_sf(coords = c("x_", "y_"), crs = 25833)
+
+HuntEvents_sf <- HuntEvents %>%
+  drop_na(HuntX, HuntY) %>%
+  dplyr::select(HuntX, HuntY) %>%
+  st_as_sf(coords = c("HuntX", "HuntY"), crs = 25833)
   
-FCM_Samples_sf <- read.csv("data/FCM Stress - Collared Deer - CRS=ETRS UTM 33N.csv", 
-                           header = TRUE, sep = ",") %>%
-  select(X, Y) %>%
-  filter(Y < max(Y) & Y > min(Y)) %>%
-  st_as_sf(coords = c("X", "Y"), crs = 32633)
+FCM_Samples_sf <- FCMStress %>%
+  dplyr::select(DefecX, DefecY) %>%
+  st_as_sf(coords = c("DefecX", "DefecY"), crs = 25833)
 
 ### get other data from OSM
 # National Park
@@ -36,55 +53,66 @@ MUC <- getbb("Munich") %>%
 
 #### Plots ####
 theme_set(theme_light() + 
-            theme(plot.title = element_text(hjust = 0.5, size = 14), 
+            theme(plot.title = element_text(hjust = 0.5, size = 16), 
                   panel.grid.major = element_blank(),
                   panel.grid.minor = element_blank(),
                   panel.border = element_blank(),
                   axis.line = element_blank(),
                   axis.text = element_blank(),
+                  axis.title = element_blank(),
                   axis.ticks = element_blank())
 )
 
 plot_overview <- ggplot(BAY) +
   geom_sf() +
-  geom_sf(data = Park$osm_multipolygons, fill = c("lightgreen", "lightblue")) +
-  # geom_sf_label(data = Park$osm_multipolygons, aes(label = c("DEU", "CZE"))) +
-  geom_sf(data = MUC$osm_points, color = "red") +
-  # geom_sf_text(data = MUC2$osm_points, color = "red", aes(label = "Munich")) +
+  geom_sf(data = Park$osm_multipolygons[1, ], fill = "lightgreen") +
+  # change color from red to black, because red is used for hunting events
+  geom_sf(data = MUC$osm_points, color = "black") +
+  geom_sf_text(data = MUC$osm_points, color = "black", aes(label = "Munich"), nudge_y = 0.1) +
   annotation_scale(location = "br") +
   annotation_north_arrow(location = "tr", which_north = "true", style = north_arrow_fancy_orienteering()) +
-  ggtitle("Location of National Park")
+  ggtitle("Location of the Bavarian Forest National Park (Green Area)")
 
-plot_Movement <- ggplot(data = Park$osm_multipolygons) + 
-  geom_sf(fill = c("lightgreen", "lightblue")) +
+# plot_Movement <- ggplot() +
+#   geom_sf(data = Park$osm_multipolygons[1, ]) +
+#   geom_sf(data = Movement_sf, aes(color = Sender.ID), alpha = .5) +
+#   guides(color = "none") +
+#   annotation_scale(location = "bl") +
+#   annotation_north_arrow(location = "tr", which_north = "true", style = north_arrow_fancy_orienteering()) +
+#   ggtitle("Deer Locations")
+
+plot_Movement <- ggplot() +
+  geom_sf(data = Park$osm_multipolygons[1, ]) +
   # geom_sf_label(data = Park$osm_multipolygons, aes(label = c("DEU", "CZE"))) +
-  geom_sf(data = Park$osm_lines) +
-  geom_sf(data = Movement_sf, aes(color = as.factor(Sender.ID)), alpha = .7) +
+  # blue for movement data -- also used in the interpolation plot
+  geom_sf(data = Movement_sf, color = "blue", alpha = .1, size = .1) +
+  facet_wrap(~Sender.ID) +
   guides(color = "none") +
   annotation_scale(location = "bl") +
   annotation_north_arrow(location = "tr", which_north = "true", style = north_arrow_fancy_orienteering()) +
-  ggtitle("Location of Deers")
+  ggtitle("Recorded Locations of 4 Random Deer") +
+  theme(strip.text = element_blank())
 
-plot_Hunts <- ggplot(data = Park$osm_multipolygons) + 
-  geom_sf(fill = c("lightgreen", "lightblue")) +
-  geom_sf(data = Park$osm_lines) +
-  geom_sf(data = HuntEvents_sf, color = "black", alpha = .7, shape = 4) +
+plot_Hunts <- ggplot() +
+  geom_sf(data = Park$osm_multipolygons[1, ]) +
+  # red for hunting events -- also used in the interpolation plot
+  geom_sf(data = HuntEvents_sf, color = "red", alpha = .5) +
   annotation_scale(location = "bl") +
   annotation_north_arrow(location = "tr", which_north = "true", style = north_arrow_fancy_orienteering()) +
-  ggtitle("Location of Hunting Events")
+  ggtitle("Hunting Event Locations")
 
-plot_samples <- ggplot(data = Park$osm_multipolygons) + 
-  geom_sf(fill = c("lightgreen", "lightblue")) +
-  geom_sf(data = Park$osm_lines) +
-  geom_sf(data = FCM_Samples_sf, color = "black", alpha = .7, shape = 2) +
+plot_samples <- ggplot() +
+  geom_sf(data = Park$osm_multipolygons[1, ]) +
+  # pruple for FCM samples -- also used in the interpolation plot
+  geom_sf(data = FCM_Samples_sf, color = "purple", alpha = .5) +
   annotation_scale(location = "bl") +
   annotation_north_arrow(location = "tr", which_north = "true", style = north_arrow_fancy_orienteering()) +
-  ggtitle("Sample Locations")
+  ggtitle("FCM Sample Locations")
 
-ggsave(plot = plot_overview, filename = "Figures/Maps/overview.png", device = "png")
-ggsave(plot = plot_Movement, filename = "Figures/Maps/Movement.png", device = "png")
-ggsave(plot = plot_Hunts, filename = "Figures/Maps/Hunts.png", device = "png")
-ggsave(plot = plot_samples, filename = "Figures/Maps/Samples.png", device = "png")
+ggsave(plot = plot_overview, filename = "Figures/Maps/overview.png", device = "png", width = 6, height = 6)
+ggsave(plot = plot_Movement, filename = "Figures/Maps/Movement.png", device = "png", width = 6, height = 6)
+ggsave(plot = plot_Hunts, filename = "Figures/Maps/Hunts.png", device = "png", width = 6, height = 6)
+ggsave(plot = plot_samples, filename = "Figures/Maps/Samples.png", device = "png", width = 6, height = 6)
 
 
 # plot_Movement | plot_Hunts | plot_samples
