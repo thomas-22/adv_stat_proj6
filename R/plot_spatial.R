3e74c481f80ff87816228aa91d2759870875d215
b-zhou <baisu.zhou@outlook.com>
2025-01-30 14:16:41 +0100

    update hunts vs deer

--- R/plot_spatial.R
+++ R/plot_spatial.R
@@ -111,18 +111,24 @@ plot_samples <- ggplot() +
   annotation_north_arrow(location = "tr", which_north = "true", style = north_arrow_fancy_orienteering()) +
   ggtitle("FCM Sample Locations")
 
-# plot_deer_vs_hunts <- ggplot() +
-#   geom_sf(data = Park$osm_multipolygons[1, ]) +
-#   # geom_sf_label(data = Park$osm_multipolygons, aes(label = c("DEU", "CZE"))) +
-#   # blue for movement data -- also used in the interpolation plot
-#   geom_sf(data = Movement_sf_selected, color = "blue", alpha = .1, size = .1) +
-#   geom_sf(data = HuntEvents_sf, color = "red", alpha = .5, size = .5) +
-#   #facet_wrap(~Sender.ID) +
-#   guides(color = "none") +
-#   annotation_scale(location = "bl") +
-#   annotation_north_arrow(location = "tr", which_north = "true", style = north_arrow_fancy_orienteering()) +
-#   ggtitle("Recorded Hunting Events vs recorded Locations of Deer") +
-#   theme(strip.text = element_blank())
+plot_deer_vs_hunts <- ggplot() +
+  geom_sf(data = Park$osm_multipolygons[1, ]) +
+  # geom_sf_label(data = Park$osm_multipolygons, aes(label = c("DEU", "CZE"))) +
+  # blue for movement data -- also used in the interpolation plot
+  geom_sf(data = Movement_sf, aes(color = "deer"), alpha = .1, size = .1) +
+  geom_sf(data = HuntEvents_sf, aes(color = "hunting event"), alpha = .5, size = .5) +
+  #facet_wrap(~Sender.ID) +
+  scale_color_manual(
+    values = c("blue", "red"),
+    labels = c("deer", "hunting event")
+  ) +
+  guides(
+    color = guide_legend(override.aes = list(alpha = 1, size = 2), title = "")
+  ) +
+  annotation_scale(location = "bl") +
+  annotation_north_arrow(location = "tr", which_north = "true", style = north_arrow_fancy_orienteering()) +
+  ggtitle("Locations of Hunting Events and Deer") +
+  theme(strip.text = element_blank(), legend.position = c(.9, .8))
 
 plot_4_deer_vs_hunts <- ggplot() +
   geom_sf(data = Park$osm_multipolygons[1, ]) +
@@ -133,7 +139,7 @@ plot_4_deer_vs_hunts <- ggplot() +
   facet_wrap(~Sender.ID) +
   scale_color_manual(
     values = c("blue", "red"),
-    labels = c("Deer", "Hunting Event")
+    labels = c("deer", "hunting event")
   ) +
   guides(
     color = guide_legend(override.aes = list(alpha = 1, size = 2), title = "")
@@ -147,7 +153,7 @@ ggsave(plot = plot_overview, filename = "Figures/Maps/overview.png", device = "p
 ggsave(plot = plot_Movement, filename = "Figures/Maps/Movement.png", device = "png", width = 6, height = 6)
 ggsave(plot = plot_Hunts, filename = "Figures/Maps/Hunts.png", device = "png", width = 6, height = 6)
 ggsave(plot = plot_samples, filename = "Figures/Maps/Samples.png", device = "png", width = 6, height = 6)
-# ggsave(plot = plot_deer_vs_hunts, filename = "Figures/Maps/deer_vs_hunts.png", device = "png", width = 6, height = 6)
+ggsave(plot = plot_deer_vs_hunts, filename = "Figures/Maps/deer_vs_hunts.png", device = "png", width = 6, height = 6)
 ggsave(plot = plot_4_deer_vs_hunts, filename = "Figures/Maps/4_deer_vs_hunts.png", device = "png", width = 6, height = 6)
 
 
